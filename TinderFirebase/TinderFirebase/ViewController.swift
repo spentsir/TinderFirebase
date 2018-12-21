d8cab71c0172dd9d9dbbe8d43fecb984ae8fc7d6
Spencer Cawley <Spent_sir@Spencers-MacBook-Pro-2.local>
2018-12-21 14:33:34 -0700

    Created Xcode project

---
+++ TinderFirebase/TinderFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderFirebase
+//
+//  Created by Spencer Cawley on 12/21/18.
+//  Copyright © 2018 Spencer Cawley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
